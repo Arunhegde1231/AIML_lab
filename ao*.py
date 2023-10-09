7ba2f7e65c82d83afa1b59d02810d013a652f2ab
Arunhegde1231 <arunhegde1231@outlook.com>
2023-10-09 06:49:45 +0530

    second commit

--- ao*.py
+++ ao*.py
@@ -1,74 +1,82 @@
-# Cost to find the AND and OR path
-def Cost(H, condition, weight = 1):
-	cost = {}
-	if 'AND' in condition:
-		AND_nodes = condition['AND']
-		Path_A = ' AND '.join(AND_nodes)
-		PathA = sum(H[node]+weight for node in AND_nodes)
-		cost[Path_A] = PathA
-
-	if 'OR' in condition:
-		OR_nodes = condition['OR']
-		Path_B =' OR '.join(OR_nodes)
-		PathB = min(H[node]+weight for node in OR_nodes)
-		cost[Path_B] = PathB
-	return cost
-
-# Update the cost
-def update_cost(H, Conditions, weight=1):
-	Main_nodes = list(Conditions.keys())
-	Main_nodes.reverse()
-	least_cost= {}
-	for key in Main_nodes:
-		condition = Conditions[key]
-		print(key,':', Conditions[key],'>>>', Cost(H, condition, weight))
-		c = Cost(H, condition, weight)
-		H[key] = min(c.values())
-		least_cost[key] = Cost(H, condition, weight)		
-	return least_cost
-
-# Print the shortest path
-def shortest_path(Start,Updated_cost, H):
-	Path = Start
-	if Start in Updated_cost.keys():
-		Min_cost = min(Updated_cost[Start].values())
-		key = list(Updated_cost[Start].keys())
-		values = list(Updated_cost[Start].values())
-		Index = values.index(Min_cost)
-		
-		# FIND MINIMIMUM PATH KEY
-		Next = key[Index].split()
-		# ADD TO PATH FOR OR PATH
-		if len(Next) == 1:
-
-			Start =Next[0]
-			Path += '<--' +shortest_path(Start, Updated_cost, H)
-		# ADD TO PATH FOR AND PATH
-		else:
-			Path +='<--('+key[Index]+') '
-
-			Start = Next[0]
-			Path += '[' +shortest_path(Start, Updated_cost, H) + ' + '
-
-			Start = Next[-1]
-			Path += shortest_path(Start, Updated_cost, H) + ']'
-
-	return Path
-		
-		
-
-H = {'A': -1, 'B': 5, 'C': 2, 'D': 4, 'E': 7, 'F': 9, 'G': 3, 'H': 0, 'I':0, 'J':0}
-
-Conditions = {
-'A': {'OR': ['B'], 'AND': ['C', 'D']},
-'B': {'OR': ['E', 'F']},
-'C': {'OR': ['G'], 'AND': ['H', 'I']},
-'D': {'OR': ['J']}
+class Graph:
+    def __init__(self,graph,heuristicNodeList,startNode):
+        self.graph=graph
+        self.H=heuristicNodeList
+        self.start=startNode
+        self.parent={}
+        self.status={}
+        self.solutionGraph={}
+        
+    def applyAOStar(self):
+        self.aoStar(self.start,False)
+    def getNeighbors(self,v):
+        return self.graph.get(v,"")
+    def getStatus(self,v):
+        return self.status.get(v,0)
+    def setStatus(self,v,val):
+        self.status[v]=val
+    def getHeuristicNodeValue(self,n):
+        return self.H.get(n,0)
+    def setHeuristicNodeValue(self,n,value):
+        self.H[n]=value
+    def printSolution(self):
+        print("FOR GRAPH SOLUTION TRAVERSE THE GRAPH FROM THE START NODE",self.start)
+        print("_________________________________________________________")
+        print(self.solutionGraph)
+        print("_________________________________________________________")
+        
+    def computeMinimumCostChildNodes(self,v):
+        minimumCost=0
+        costToChildNodeListDict={}
+        costToChildNodeListDict[minimumCost]=[]
+        flag=True
+        for nodeInfoTupleList in self.getNeighbors(v):
+            cost=0
+            nodeList=[]
+            for c, weight in nodeInfoTupleList:
+                cost=cost+self.getHeuristicNodeValue(c)+weight
+                nodeList.append(c)
+            if flag==True:
+                minimumCost=cost
+                costToChildNodeListDict[minimumCost]=nodeList
+                flag=False
+            else:
+                if minimumCost>cost:
+                    minimumCost=cost
+                    costToChildNodeListDict[minimumCost]=nodeList
+        return minimumCost,costToChildNodeListDict[minimumCost]
+    def aoStar(self,v,backTracking):
+        print("HEURISTIC VALUES :",self.H)
+        print("SOLUTION GRAPH :",self.solutionGraph)
+        print("PROCESSING NODE :",v)
+        print("_____________________________________")
+        if self.getStatus(v)>=0:
+            minimumCost,childNodeList=self.computeMinimumCostChildNodes(v)
+            self.setHeuristicNodeValue(v,minimumCost)
+            self.setStatus(v,len(childNodeList))
+            solved=True
+            for childNode in childNodeList:
+                self.parent[childNode]=v
+                if self.getStatus(childNode)!=-1:
+                    solved=solved & False
+            if solved==True:
+                self.setStatus(v,-1)
+                self.solutionGraph[v]=childNodeList
+            if v!=self.start:
+                self.aoStar(self.parent[v],True)
+            if backTracking==False:
+                for childNode in childNodeList:
+                    self.setStatus(childNode,0)
+                    self.aoStar(childNode,False)
+                    
+h1={'A':1,'B':6,'C':2,'D':12,'E':2,'F':1,'G':5,'H':7,'I':7,'J':1,'T':3}
+graph1={
+    'A':[[('B',1),('C',1)],[('D',1)]],
+    'B':[[('G',1)],[('H',1)]],
+    'C':[[('J',1)]],
+    'D':[[('E',1),('F',1)]],
+    'G':[[('I',1)]]
 }
-# weight
-weight = 1
-# Updated cost
-print('Updated Cost :')
-Updated_cost = update_cost(H, Conditions, weight=1)
-print('*'*75)
-print('Shortest Path :\n',shortest_path('A', Updated_cost,H))
+G1=Graph(graph1,h1,'A')
+G1.applyAOStar()
+G1.printSolution()
