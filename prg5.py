9b9037e992f15481044ef592690867e922979889
Arun Hegde <arunhegde1231@outlook.com>
2024-01-02 22:05:05 +0530

    formatting changes

--- prg5.py
+++ prg5.py
@@ -1,8 +1,8 @@
 import numpy as np
 X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
-y = np.array(([92], [86], [89]), dtype=float)
+Y = np.array(([92], [86], [89]), dtype=float)
 X = X/np.amax(X,axis=0) 
-y = y/100
+Y = Y/100
 
 def sigmoid (x):
     return 1/(1 + np.exp(-x))
@@ -27,12 +27,12 @@ for i in range(epoch):
     hinp1=np.dot(X,wh)
     hinp=hinp1 + bh
     hlayer_act = sigmoid(hinp)
-    
+
     outinp1=np.dot(hlayer_act,wout)
     outinp= outinp1+ bout
     output = sigmoid(outinp)
     
-    EO = y-output
+    EO = Y-output
     outgrad = derivatives_sigmoid(output)
     d_output = EO* outgrad
     
@@ -44,5 +44,5 @@ for i in range(epoch):
     wh += X.T.dot(d_hiddenlayer) *lr
     
 print("Input: \n" + str(X))
-print("Actual Output: \n" + str(y))
+print("Actual Output: \n" + str(Y))
 print("Predicted Output: \n" ,output)
