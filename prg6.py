ecf3ff23e9a76fefbcd8e57e6dab9718d527edf3
Arun Hegde <arunhegde1231@outlook.com>
2024-01-02 20:00:19 +0530

    modified prg 6

--- prg6.py
+++ prg6.py
@@ -1,17 +1,91 @@
-from sklearn.naive_bayes import GaussianNB
-from sklearn.datasets import load_iris
-from sklearn import metrics
-from sklearn.model_selection import train_test_split as s
+import csv
+import random
+import math
 
-dataset=load_iris()
-x=dataset.data
-y=dataset.target
-x_train,x_test, y_train, y_test= s(x,y,test_size=0.2, random_state=1)
-gnb=GaussianNB()
-classifier=gnb.fit(x_train,y_train)
-y_pred=classifier.predict(x_test)
+def splitDataset(dataset, splitRatio):
+    trainSize = int(len(dataset) * splitRatio)
+    trainSet = []
+    copy = list(dataset)
+    while len(trainSet) < trainSize:
+        data_index = random.randrange(len(copy))
+        trainSet.append(copy.pop(data_index))
+    return [trainSet, copy]
 
-print("accuracy matrices : \n" ,metrics.classification_report(y_test,y_pred))
-print("accuracy of matrices is : ", metrics.accuracy_score(y_test,y_pred))
-print("confusion matrix ")
-print("accuracy matrices : ", metrics.confusion_matrix(y_test,y_pred))
+def separateByClass(dataset):
+    separated = {}
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        if vector[-1] not in separated:
+            separated[vector[-1]] = []
+        separated[vector[-1]].append(vector)
+    return separated
+
+def mean(numbers):
+    return sum(numbers) / float(len(numbers))
+
+def stdev(numbers):
+    avg = mean(numbers)
+    variance = sum([pow(x - avg, 2) for x in numbers]) / float(len(numbers) - 1)
+    return math.sqrt(variance)
+
+def summarize(dataset):
+    summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*dataset)]
+    del summaries[-1]
+    return summaries
+
+def summarizeByClass(dataset):
+    separated = separateByClass(dataset)
+    summaries = {}
+    for classValue, instances in separated.items():
+        summaries[classValue] = summarize(instances)
+    return summaries
+
+def calculateProbability(x, mean, stdev):
+    exponent = math.exp(-(math.pow(x - mean, 2) / (2 * math.pow(stdev, 2))))
+    return (1 / (math.sqrt(2 * math.pi) * stdev)) * exponent
+
+def calculateClassProbabilities(summaries, inputVector):
+    probabilities = {}
+    for classValue, classSummaries in summaries.items():
+        probabilities[classValue] = 1
+        for i in range(len(classSummaries)):
+            mean, stdev = classSummaries[i]
+            x = inputVector[i]
+            probabilities[classValue] *= calculateProbability(x, mean, stdev)
+    return probabilities
+
+def predict(summaries, inputVector):
+    probabilities = calculateClassProbabilities(summaries, inputVector)
+    bestLabel, bestProb = None, -1
+    for classValue, probability in probabilities.items():
+        if bestLabel is None or probability > bestProb:
+            bestProb = probability
+            bestLabel = classValue
+    return bestLabel
+
+def getPredictions(summaries, testSet):
+    predictions = []
+    for i in range(len(testSet)):
+        result = predict(summaries, testSet[i])
+        predictions.append(result)
+    return predictions
+
+def getAccuracy(testSet, predictions):
+    correct = 0
+    for i in range(len(testSet)):
+        if testSet[i][-1] == predictions[i]:
+            correct += 1
+    return (correct / float(len(testSet))) * 100.0
+
+f = open('p6.csv') 
+lines = csv.reader(f)
+dataset = list(lines)
+for i in range(len(dataset)):
+    dataset[i] = [float(x) for x in dataset[i]]
+splitRatio = 0.67
+trainingSet, testSet = splitDataset(dataset, splitRatio)
+print('Split {0} rows into train={1} and test={2} rows'.format(len(dataset), len(trainingSet), len(testSet)))
+summaries = summarizeByClass(trainingSet)
+predictions = getPredictions(summaries, testSet)
+accuracy = getAccuracy(testSet, predictions)
+print('Accuracy of the classifier is: {0}%'.format(accuracy))
